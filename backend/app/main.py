b92f46b472408a914a71ccfea74090022d60430d
Kellen Wynn <kellenwynn11@gmail.com>
2026-04-26 01:13:33 -0400

    temporarily allow all cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,16 +21,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_origin_regex=r"https://.*\.vercel\.app",
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 Base.metadata.create_all(bind=engine)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
